647a5be55804cb7c4b337f154700f5b20c10165d
afeiship <1290657123@qq.com>
2019-06-15 14:21:28 +0800

    add: test lint-staged-pretieer

--- src/components/delay.js
+++ src/components/delay.js
@@ -3,7 +3,7 @@ export default function(inInterval) {
     setTimeout(function() {
       // 模拟出错了，返回 ‘error’
       // reject('error');
-      var ab = 12;
+var ab = 12;
       console.log(ab);
       resolve();
     }, inInterval);
