01ef3ccdee4be9e2d19101aa9cf7aa5f8e621270
afeiship <1290657123@qq.com>
2019-06-15 14:25:02 +0800

    test: pretier & commit

--- src/components/delay.js
+++ src/components/delay.js
@@ -3,8 +3,8 @@ export default function(inInterval) {
     setTimeout(function() {
       // 模拟出错了，返回 ‘error’
       // reject('error');
-var ab = 12;
-      console.log(ab);
+      var ab = 12
+console.log(ab);
       resolve();
     }, inInterval);
   });
