236d714c4bdbe69957aa2e39b41612479d247539
afeiship <1290657123@qq.com>
2019-06-15 14:31:57 +0800

    test: eslint fix

--- src/components/delay.js
+++ src/components/delay.js
@@ -4,7 +4,7 @@ export default function(inInterval) {
       // 模拟出错了，返回 ‘error’
       // reject('error');
       var ab = 12;
-console.log(ab);
+      console.log(ab)
       resolve();
     }, inInterval);
   });
