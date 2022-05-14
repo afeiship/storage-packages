4daf595cb79d6f23bccf82e2725b4a7bf18cfae1
afeiship <1290657123@qq.com>
2022-05-14 09:55:33 +0800

    feat: sass + dart-sass

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,21 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'), //path为打包后的输出文件夹位置，此处为 ./dist文件夹
     filename: 'bundle.js'
   },
-
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // 将 JS 字符串生成为 style 节点
+          'style-loader',
+          // 将 CSS 转化成 CommonJS 模块
+          'css-loader',
+          // 将 Sass 编译成 CSS
+          'sass-loader',
+        ],
+      },
+    ],
+  },
   plugins: [
     new HtmlWebpackPlugin({
       template: './public/index.html'
