50d2d5e9de8223caf190d804ad09c6afb934d513
afeiship <1290657123@qq.com>
2023-07-13 20:30:56 +0800

    refactor: upt localstorage

--- packages/next-local-storage/src/index.js
+++ packages/next-local-storage/src/index.js
@@ -1,21 +1,20 @@
-(function () {
-  var global = global || this || window || Function('return this')();
-  var nx = global.nx || require('@jswork/next');
-  var NxAbstractStorage = nx.AbstractStorage || require('@jswork/next-abstract-storage');
+import nx from '@jswork/next';
+import '@jswork/next-abstract-storage';
 
-  var NxLocalStorage = nx.declare('nx.LocalStorage', {
-    extends: NxAbstractStorage,
-    methods: {
-      init: function (inPrefix) {
-        this.$base.init.call(this, {
-          engine: global.localStorage,
-          prefix: inPrefix || ''
-        });
-      }
+const NxLocalStorage = nx.declare('nx.LocalStorage', {
+  extends: nx.AbstractStorage,
+  methods: {
+    init: function (inPrefix) {
+      this.$base.init.call(this, {
+        engine: global.localStorage,
+        prefix: inPrefix || ''
+      });
     }
-  });
-
-  if (typeof module !== 'undefined' && module.exports) {
-    module.exports = NxLocalStorage;
   }
-})();
+});
+
+if (typeof module !== 'undefined' && module.exports && typeof wx === 'undefined') {
+  module.exports = NxLocalStorage;
+}
+
+export default NxLocalStorage;
