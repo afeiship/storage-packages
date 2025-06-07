c4e06cd985d50486725f61e4e60d3b91971adf47
afeiship <1290657123@qq.com>
2025-06-07 14:50:19 +0800

    feat: nx.nil & null

--- packages/next-abstract-storage/src/index.js
+++ packages/next-abstract-storage/src/index.js
@@ -1,17 +1,17 @@
-import nx from '@jswork/next';
-import '@jswork/next-json';
-import '@jswork/next-slice2str';
+import nx from '@jswork/next'
+import '@jswork/next-json'
+import '@jswork/next-slice2str'
 
-const EMPTY_STR = '';
-const SEPARATOR = '@';
+const EMPTY_STR = ''
+const SEPARATOR = '@'
 
 const NxAbstractStorage = nx.declare('nx.AbstractStorage', {
   methods: {
     init: function (inOptions) {
-      this.engine = inOptions.engine;
-      this.prefix = inOptions.prefix || EMPTY_STR;
-      this.options = inOptions;
-      this.setAccessor();
+      this.engine = inOptions.engine
+      this.prefix = inOptions.prefix || EMPTY_STR
+      this.options = inOptions
+      this.setAccessor()
     },
     setAccessor: function () {
       this.accessor = {
@@ -20,113 +20,113 @@ const NxAbstractStorage = nx.declare('nx.AbstractStorage', {
         remove: this.options.remove || 'removeItem',
         clear: this.options.clear || 'clear',
         save: this.options.save || 'save'
-      };
+      }
     },
     serialize: function (inTarget) {
-      return nx.stringify(inTarget);
+      return nx.stringify(inTarget)
     },
     deserialize: function (inString) {
-      return nx.parse(inString);
+      return nx.parse(inString)
     },
     set: function (inKey, inValue) {
       // del when nx.NIL
-      if (inValue === nx.NIL) return this.del(inKey);
+      if (inValue === nx.NIL || inValue === null) return this.del(inKey)
       // else set
-      var index = inKey.indexOf('.');
+      var index = inKey.indexOf('.')
       if (index > -1) {
-        var paths = nx.slice2str(inKey, index, 1);
-        var context = this.get(paths[0]) || {};
-        nx.set(context, paths[1], inValue);
-        this.set(paths[0], context);
+        var paths = nx.slice2str(inKey, index, 1)
+        var context = this.get(paths[0]) || {}
+        nx.set(context, paths[1], inValue)
+        this.set(paths[0], context)
       } else {
-        this.engine[this.accessor.set](this.__key(inKey), this.serialize(inValue));
+        this.engine[this.accessor.set](this.__key(inKey), this.serialize(inValue))
       }
-      this.save();
+      this.save()
     },
     sets: function (inObject) {
       nx.each(
         inObject,
         function (key, value) {
-          this.set(key, value);
+          this.set(key, value)
         },
         this
-      );
+      )
     },
     get: function (inKey) {
-      var index = inKey.indexOf('.');
+      var index = inKey.indexOf('.')
       if (index > -1) {
-        var paths = nx.slice2str(inKey, index, 1);
-        var context = this.get(paths[0]) || {};
-        return nx.get(context, paths[1]);
+        var paths = nx.slice2str(inKey, index, 1)
+        var context = this.get(paths[0]) || {}
+        return nx.get(context, paths[1])
       } else {
-        var value = this.engine[this.accessor.get](this.__key(inKey));
-        return this.deserialize(value);
+        var value = this.engine[this.accessor.get](this.__key(inKey))
+        return this.deserialize(value)
       }
     },
     gets: function (inKeys) {
-      var result = {};
-      var keys = this.__keys(inKeys);
+      var result = {}
+      var keys = this.__keys(inKeys)
       nx.each(
         keys,
         function (_, key) {
-          result[key] = this.get(key);
+          result[key] = this.get(key)
         },
         this
-      );
-      return result;
+      )
+      return result
     },
     del: function (inKey) {
-      this.engine[this.accessor.remove](this.__key(inKey));
-      this.save();
+      this.engine[this.accessor.remove](this.__key(inKey))
+      this.save()
     },
     dels: function (inKeys) {
-      var keys = this.__keys(inKeys);
+      var keys = this.__keys(inKeys)
       nx.each(
         keys,
         function (_, key) {
-          this.del(key);
+          this.del(key)
         },
         this
-      );
+      )
     },
     clear: function () {
-      this.engine[this.accessor.clear]();
-      this.save();
+      this.engine[this.accessor.clear]()
+      this.save()
     },
     keys: function () {
-      return Object.keys(this.engine);
+      return Object.keys(this.engine)
     },
     save: function () {
       // @template method
     },
     __key: function (inKey) {
-      var prefix = this.prefix;
-      return prefix ? [prefix, SEPARATOR, inKey].join(EMPTY_STR) : inKey;
+      var prefix = this.prefix
+      return prefix ? [prefix, SEPARATOR, inKey].join(EMPTY_STR) : inKey
     },
     __keys: function (inKeys) {
-      var length_, keys;
-      var allNsKeys = [];
+      var length_, keys
+      var allNsKeys = []
       if (!Array.isArray(inKeys)) {
-        keys = this.keys();
-        length_ = this.prefix.length + 1;
+        keys = this.keys()
+        length_ = this.prefix.length + 1
         nx.each(
           keys,
           function (_, item) {
             if (this.prefix && item.indexOf(this.prefix + SEPARATOR) === 0) {
-              allNsKeys.push(item.slice(length_));
+              allNsKeys.push(item.slice(length_))
             }
           },
           this
-        );
-        return allNsKeys.length ? allNsKeys : keys;
+        )
+        return allNsKeys.length ? allNsKeys : keys
       }
-      return inKeys;
+      return inKeys
     }
   }
-});
+})
 
 if (typeof module !== 'undefined' && module.exports && typeof wx === 'undefined') {
-  module.exports = NxAbstractStorage;
+  module.exports = NxAbstractStorage
 }
 
-export default NxAbstractStorage;
+export default NxAbstractStorage
